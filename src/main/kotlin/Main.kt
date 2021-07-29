90a79738b95c9e1363a9041c4c54e8e26b6d2834
will.109 <will.109@kakaopaycorp.com>
2021-07-29 10:25:50 +0900

    주석 수정

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,19 +1,18 @@
 //뒷북인데 Companion이라고 써있어서 응? Companion 클래스의
-@JvmStatic
-fun method() {
-    print("static")
-}
-
-//class TextClass {
-//    companion object {
-//        @JvmStatic
-//        fun method() {
-//            print("static")
-//        }
-//    }
+//@JvmStatic
+//fun method() {
+//    print("static")
 //}
 
+class MyClass {
+    companion object {
+        @JvmStatic
+        fun method() {
+            print("static")
+        }
+    }
+}
 
 fun main() {
-    method()
+    MyClass.method()
 }
